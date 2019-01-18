9b861d1bb47244034129495b881b2eb5670137d9
nayanprakash <nayanprakash@nayanXPS.localdomain>
2019-01-18 09:02:14 -0700

    Finish Game Logic and begin bot

--- server/create_board.py
+++ server/create_board.py
@@ -1,12 +1,13 @@
 from node import Node
 import random
 
-def generate(size):
+def generate(size, seed):
     """
     Create the Initial Game Board and Catch Basins
 
     Args:
         size: total amount of nodes in board
+        seed: seed for psuedorandom numbers
 
     Returns:
         board: an array of node objects
@@ -16,6 +17,8 @@ def generate(size):
     for i in range(sqrt(size)):
         for j in range(sqrt(size)):
             board[i][j] = Node()
+
+    random.seed(seed)
 
     for i in range(sqrt(size)):
         x = random.randint(0, sqrt(size)-1)
