d23ab6840e5b5173c1c00a5a2292568fae7a58c9
Kyle Hennig <henn.kyle@outlook.com>
2019-01-18 09:30:10 -0700

    Fix capitalization of boolean constants

--- server/node.py
+++ server/node.py
@@ -2,15 +2,15 @@ class Node:
     """Node objects that makes up board"""
 
     def __init__(self):
-        self.visited = false
-        self.basin = false
+        self.visited = False
+        self.basin = False
         self.adjacent = 0
 
     def set_basin(self):
-        self.basin = true
+        self.basin = True
 
     def set_visited(self):
-        self.visited = true
+        self.visited = True
 
     def increment_adjacent(self):
         self.adjacent += 1
